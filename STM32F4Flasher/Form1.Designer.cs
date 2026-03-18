81a3bca9cdd41b0e2921a3b98458ed5e998670a9
SuleDemirdas <109147185+SuleDemirdas@users.noreply.github.com>
2026-03-19 01:52:23 +0300

    Implement connection group

--- STM32F4Flasher/Form1.Designer.cs
+++ STM32F4Flasher/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace STM32F4Flasher
 {
-    partial class Form1
+    partial class STM32F4Flasher
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            Connection = new GroupBox();
+            progressBar = new ProgressBar();
+            connectionStatus = new Label();
+            buttonDisconnect = new Button();
+            buttonConnect = new Button();
+            comBoxParity = new ComboBox();
+            comBoxStopBits = new ComboBox();
+            lblParity = new Label();
+            lblStopBits = new Label();
+            comBoxBaudrate = new ComboBox();
+            lblBaudrate = new Label();
+            comBoxComPort = new ComboBox();
+            lblComPort = new Label();
+            groupBox1 = new GroupBox();
+            Connection.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Connection
+            // 
+            Connection.Controls.Add(progressBar);
+            Connection.Controls.Add(connectionStatus);
+            Connection.Controls.Add(buttonDisconnect);
+            Connection.Controls.Add(buttonConnect);
+            Connection.Controls.Add(comBoxParity);
+            Connection.Controls.Add(comBoxStopBits);
+            Connection.Controls.Add(lblParity);
+            Connection.Controls.Add(lblStopBits);
+            Connection.Controls.Add(comBoxBaudrate);
+            Connection.Controls.Add(lblBaudrate);
+            Connection.Controls.Add(comBoxComPort);
+            Connection.Controls.Add(lblComPort);
+            Connection.Location = new Point(12, 18);
+            Connection.Name = "Connection";
+            Connection.Size = new Size(327, 299);
+            Connection.TabIndex = 0;
+            Connection.TabStop = false;
+            Connection.Text = "Connection";
+            Connection.Enter += groupBox1_Enter;
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(130, 238);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(179, 17);
+            progressBar.TabIndex = 9;
+            // 
+            // connectionStatus
+            // 
+            connectionStatus.AutoSize = true;
+            connectionStatus.Location = new Point(182, 199);
+            connectionStatus.Name = "connectionStatus";
+            connectionStatus.Size = new Size(99, 20);
+            connectionStatus.TabIndex = 1;
+            connectionStatus.Text = "Disconnected";
+            connectionStatus.Click += connectionStatus_Click;
+            // 
+            // buttonDisconnect
+            // 
+            buttonDisconnect.Location = new Point(6, 249);
+            buttonDisconnect.Name = "buttonDisconnect";
+            buttonDisconnect.Size = new Size(94, 29);
+            buttonDisconnect.TabIndex = 1;
+            buttonDisconnect.Text = "Disconnect";
+            buttonDisconnect.UseVisualStyleBackColor = true;
+            buttonDisconnect.Click += buttonDisconnect_Click;
+            // 
+            // buttonConnect
+            // 
+            buttonConnect.Location = new Point(6, 199);
+            buttonConnect.Name = "buttonConnect";
+            buttonConnect.Size = new Size(94, 29);
+            buttonConnect.TabIndex = 8;
+            buttonConnect.Text = "Connect";
+            buttonConnect.UseVisualStyleBackColor = true;
+            buttonConnect.Click += buttonConnect_Click;
+            // 
+            // comBoxParity
+            // 
+            comBoxParity.FormattingEnabled = true;
+            comBoxParity.Items.AddRange(new object[] { "None", "Even", "Odd" });
+            comBoxParity.Location = new Point(87, 149);
+            comBoxParity.Name = "comBoxParity";
+            comBoxParity.Size = new Size(151, 28);
+            comBoxParity.TabIndex = 7;
+            comBoxParity.Text = "None";
+            comBoxParity.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
+            // 
+            // comBoxStopBits
+            // 
+            comBoxStopBits.FormattingEnabled = true;
+            comBoxStopBits.Items.AddRange(new object[] { "1", "2" });
+            comBoxStopBits.Location = new Point(87, 115);
+            comBoxStopBits.Name = "comBoxStopBits";
+            comBoxStopBits.Size = new Size(151, 28);
+            comBoxStopBits.TabIndex = 6;
+            comBoxStopBits.Text = "1";
+            comBoxStopBits.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // lblParity
+            // 
+            lblParity.AutoSize = true;
+            lblParity.Location = new Point(12, 153);
+            lblParity.Name = "lblParity";
+            lblParity.Size = new Size(45, 20);
+            lblParity.TabIndex = 5;
+            lblParity.Text = "Parity";
+            lblParity.Click += label2_Click;
+            // 
+            // lblStopBits
+            // 
+            lblStopBits.AutoSize = true;
+            lblStopBits.Location = new Point(12, 115);
+            lblStopBits.Name = "lblStopBits";
+            lblStopBits.Size = new Size(68, 20);
+            lblStopBits.TabIndex = 4;
+            lblStopBits.Text = "Stop Bits";
+            lblStopBits.Click += label1_Click_1;
+            // 
+            // comBoxBaudrate
+            // 
+            comBoxBaudrate.FormattingEnabled = true;
+            comBoxBaudrate.Items.AddRange(new object[] { "115200", "9600" });
+            comBoxBaudrate.Location = new Point(87, 68);
+            comBoxBaudrate.Name = "comBoxBaudrate";
+            comBoxBaudrate.Size = new Size(151, 28);
+            comBoxBaudrate.TabIndex = 3;
+            comBoxBaudrate.Text = "115200";
+            comBoxBaudrate.SelectedIndexChanged += comboBox1_SelectedIndexChanged_1;
+            // 
+            // lblBaudrate
+            // 
+            lblBaudrate.AutoSize = true;
+            lblBaudrate.Location = new Point(12, 71);
+            lblBaudrate.Name = "lblBaudrate";
+            lblBaudrate.Size = new Size(69, 20);
+            lblBaudrate.TabIndex = 2;
+            lblBaudrate.Text = "Baudrate";
+            lblBaudrate.Click += label1_Click;
+            // 
+            // comBoxComPort
+            // 
+            comBoxComPort.FormattingEnabled = true;
+            comBoxComPort.Location = new Point(87, 26);
+            comBoxComPort.Name = "comBoxComPort";
+            comBoxComPort.Size = new Size(151, 28);
+            comBoxComPort.TabIndex = 1;
+            comBoxComPort.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // lblComPort
+            // 
+            lblComPort.AutoSize = true;
+            lblComPort.Location = new Point(12, 29);
+            lblComPort.Name = "lblComPort";
+            lblComPort.Size = new Size(42, 20);
+            lblComPort.TabIndex = 0;
+            lblComPort.Text = "COM";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(351, 18);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(509, 489);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Commands";
+            // 
+            // STM32F4Flasher
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(869, 519);
+            Controls.Add(groupBox1);
+            Controls.Add(Connection);
+            Name = "STM32F4Flasher";
+            Text = "STM32F4Flasher";
+            Load += Form1_Load;
+            Connection.ResumeLayout(false);
+            Connection.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox Connection;
+        private ComboBox comBoxComPort;
+        private Label lblComPort;
+        private Label lblBaudrate;
+        private ComboBox comBoxBaudrate;
+        private Label lblStopBits;
+        private Label lblParity;
+        private ComboBox comBoxParity;
+        private ComboBox comBoxStopBits;
+        private Button buttonDisconnect;
+        private Button buttonConnect;
+        private Label connectionStatus;
+        private ProgressBar progressBar;
+        private GroupBox groupBox1;
     }
 }
--- STM32F4Flasher/Form1.cs
+++ STM32F4Flasher/Form1.cs
@@ -1,10 +1,113 @@
+using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+
 namespace STM32F4Flasher
 {
-    public partial class Form1 : Form
+    public partial class STM32F4Flasher : Form
     {
-        public Form1()
+        SerialPort serialPort1 = new SerialPort();
+        public STM32F4Flasher()
         {
             InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] ports = SerialPort.GetPortNames();
+            comBoxComPort.Items.AddRange(ports);
+
+            buttonConnect.Enabled = true;
+            buttonDisconnect.Enabled = false;
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comBoxComPort.Text;
+            serialPort1.BaudRate = Int32.Parse(comBoxBaudrate.Text);
+            serialPort1.Parity = comBoxParity.Text == "None" ? Parity.None :
+                                 comBoxParity.Text == "Even" ? Parity.Even :
+                                 comBoxParity.Text == "Odd" ? Parity.Odd : Parity.None;
+            serialPort1.StopBits = comBoxStopBits.Text == "1" ? StopBits.One :
+                                    comBoxStopBits.Text == "1.5" ? StopBits.OnePointFive :
+                                    comBoxStopBits.Text == "2" ? StopBits.Two : StopBits.One;
+
+            try
+            {
+                serialPort1.Open();
+                buttonConnect.Enabled = false;
+                buttonDisconnect.Enabled = true;
+                comBoxComPort.Enabled = false;
+                connectionStatus.Text = "Connected";
+                progressBar.Value = 100;
+            }
+            catch
+            {
+                MessageBox.Show("Failed to connect to the selected COM port. Please check your settings and try again.", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                buttonConnect.Enabled = true;
+                buttonDisconnect.Enabled = false;
+                connectionStatus.Text = "Connection Failed";
+            }
+        }
+
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connectionStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                buttonConnect.Enabled = true;
+                buttonDisconnect.Enabled = false;
+                comBoxComPort.Enabled = true;
+                connectionStatus.Text = "Disconnected";
+                progressBar.Value = 0;
+            }
         }
     }
 }
